4e86f64e9fbc82626dda17574609175f89ba5c04
nawre <naoures.brahim@cruxsoft.be>
2024-08-27 17:10:56 +0200

    Add useFetchBook hook

---
+++ src/hooks/useFetchBooks.js
@@ -0,0 +1,103 @@
+import {useEffect, useState} from "react";
+
+const useFetchBooks = () => {
+    const books = [
+        {
+            id: "1",
+            title: "THE HOUSEMAID IS WATCHING",
+            author: "MCFADDEN, FREIDA",
+            price: 45
+
+        },
+        {
+            id: "2",
+            title: "ERUPTION",
+            author: "CRICHTON, MICHAEL",
+            price: 50
+
+        },
+        {
+            id: "3",
+            title: "SWAN SONG",
+            author: "HILDERBRAND, ELIN",
+            price: 38
+        },
+        {
+            id: "4",
+            title: "DAD, I WANT TO HEAR YOUR STORY",
+            author: "MASON, JEFFREY",
+            price: 42
+
+        },
+        {
+            id: "5",
+            title: "THE WOMEN",
+            author: "HANNAH, KRISTIN",
+            price: 65
+
+        },
+        {
+            id: "6",
+            title: "THE HOUSEMAID",
+            author: "MCFADDEN, FREIDA",
+            price: 65
+
+        },
+        {
+            id: "7",
+            title: "A COURT OF THORNS AND ROSES",
+            author: "MAAS, SARAH J.",
+            price: 65
+
+        },
+        {
+            id: "8",
+            title: "CAMINO GHOSTS",
+            author: "GRISHAM, JOHN",
+            price: 65
+
+        },
+        {
+            id: "9",
+            title: "RECKIESS THE POWERIESS TRILOGY",
+            author: "NADIR, AL M.",
+            price: 65
+
+        },
+        {
+            id: "10",
+            title: "DOG MAN: THE SCARLET SHEDDER",
+            author: "PILKEY, DAV",
+            price: 65
+
+        }
+    ]
+    const url = `https://fakestoreapi.com/Books?description_like`
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState();
+
+    useEffect(() => {
+        setLoading(false);
+        setError(false);
+
+        fetch(url)
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then((result) => setData(result))
+            .catch((err) => setError(err))
+            .finally(() => setLoading(false));
+    }, [url]);
+
+    return {
+        data: books,
+        loading ,
+        error:false
+    };
+};
+
+export default useFetchBooks;
